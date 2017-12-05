a400cf8e6d0796f34bbdbaac1938f132a274c474
Xue <tianxueol@outlook.com>
2017-12-05 17:18:32 +0800

    update main.py

--- ArticleCollect/ArticleCollect/main.py
+++ ArticleCollect/ArticleCollect/main.py
@@ -1,4 +1,10 @@
 from scrapy.cmdline import execute
 
 # execute('scrapy crawl malwarebytes'.split(' '))
-execute('scrapy crawl jpcert'.split(' '))
+# execute('scrapy crawl jpcert'.split(' '))
+# execute('scrapy crawl fireeye'.split(' '))
+# execute('scrapy crawl alienvault'.split(' '))
+# execute('scrapy crawl phishme'.split(' '))
+# execute('scrapy crawl malwarepatrol'.split(' '))
+# execute('scrapy crawl badcyber'.split(' '))
+execute('scrapy crawl virustotal'.split(' '))
